0cb6f358ba73047a7dddcecdcbc3b37419f3e0c6
hpc <fortgu@163.com>
2024-06-20 20:54:27 +0800

    1

--- models/GMN-RF/model.py
+++ models/GMN-RF/model.py
@@ -1,3 +1,7 @@
+import torch.nn as nn
+
+from src.gconv import Siamese_Gconv
+from models.PCA.affinity_layer import Affinity
 from src.lap_solvers.sinkhorn import Sinkhorn
 from src.utils.config import cfg
 
@@ -8,3 +12,22 @@ class Net(CNN):
     def __init__(self):
         super(Net, self).__init__()
         self.sinkhorn = Sinkhorn(max_iter=cfg.PCA.SK_ITER_NUM, epsilon=cfg.PCA.SK_EPSILON, tau=cfg.PCA.SK_TAU)
+        self.l2norm = nn.LocalResponseNorm(cfg.PCA.FEATURE_CHANNEL * 2, alpha=cfg.PCA.FEATURE_CHANNEL * 2, beta=0.5,
+                                           k=0)
+        self.gnn_layer = cfg.PCA.GNN_LAYER
+        for i in range(self.gnn_layer):
+            if i == 0:
+                gnn_layer = Siamese_Gconv(cfg.PCA.FEATURE_CHANNEL * 2, cfg.PCA.GNN_FEAT)
+            else:
+                gnn_layer = Siamese_Gconv(cfg.PCA.GNN_FEAT, cfg.PCA.GNN_FEAT)
+            self.add_module('gnn_layer_{}'.format(i), gnn_layer)
+            self.add_module('affinity_{}'.format(i), Affinity(cfg.PCA.GNN_FEAT))
+            if i == self.gnn_layer - 2:  # only second last layer will have cross-graph module
+                self.add_module('cross_graph_{}'.format(i), nn.Linear(cfg.PCA.GNN_FEAT * 2, cfg.PCA.GNN_FEAT))
+        self.cross_iter = cfg.PCA.CROSS_ITER
+        self.cross_iter_num = cfg.PCA.CROSS_ITER_NUM
+        self.rescale = cfg.PROBLEM.RESCALE
+
+        def reload_backbone(self):
+            self.node_layers, self.edge_layers = self.get_backbone(True)
+
